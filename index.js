854024fd0e3d530ac332859f931baf4a3c17f0a6
D7_49611_Pradnya <pradnyakasar1913@gmail.com>
2022-05-11 10:08:37 +0530

    excel_sheet_code_added

--- index.js
+++ index.js
@@ -75,17 +75,37 @@
 
 const moment = require("moment");
 
+const XLSX = require("xlsx");
+
 const whoisinfo = require("whois-json");
+
+const fs = require("fs");
+
+const extractDomain = require("extract-domain");
+
+const isValidDomain = require("is-valid-domain");
+
+var spread_sheet = require("spread_sheet");
+
+var json2xls = require("json2xls");
 
 const bodyParser = require("body-parser");
 
-var isValidDomain = require("is-valid-domain");
+const ExcelJS = require("exceljs");
+
+const wb = new ExcelJS.Workbook();
 
 const express = require("express");
+
+const { exec } = require("child_process");
 
 const app = express();
 
 app.use(bodyParser.json());
+
+let domains = [];
+
+let fileEmpty = true;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
@@ -98,6 +118,7 @@ app.get("/", (req, res) => {
     flag: false,
     date: "",
     domainAge: "",
+    invalid: false,
   });
 });
 
@@ -116,13 +137,52 @@ app.get("/domainagechecker", (req, res) => {
   });
 });
 
+function removeHttp(url) {
+  if (url.startsWith("https://www.")) {
+    const http = "https://www.";
+    return url.slice(http.length);
+  }
+
+  if (url.startsWith("http://www.")) {
+    const http = "http://www.";
+    return url.slice(http.length);
+  }
+
+  if (url.startsWith("https://")) {
+    const https = "https://";
+    return url.slice(https.length);
+  }
+
+  if (url.startsWith("http://")) {
+    const http = "http://";
+    return url.slice(http.length);
+  }
+
+  if (url.startsWith("www.")) {
+    const http = "www.";
+    return url.slice(http.length);
+  }
+
+  return url;
+}
+
 app.post("/domainwhoisinfo", async (req, res) => {
-  var domain = req.body.domain;
+  var domain = removeHttp(req.body.domain);
+  console.log(domain);
   if (isValidDomain(domain)) {
     console.log(domain);
     var results = await whoisinfo(domain);
     console.log(results);
-    // res.render("domainwhoisinfo", { data: response });
+    // const students = [
+    //   {
+    //     name: Response.domainName,
+    //     email: Response.creationDate,
+    //     age: Response.registrarRegistrationExpirationDate,
+    //     gender: Response.domainAge,
+    //   },
+    //   { name: "Rahul", email: "rahul@gmail.com", age: 15, gender: "M" },
+    // ];
+
     var date = moment(results.creationDate).format("YYYY-MM-DD");
     var currentDate = moment(new Date()).format("YYYY-MM-DD");
 
@@ -148,6 +208,19 @@ app.post("/domainwhoisinfo", async (req, res) => {
 
     //console.log(year + "-" + month + "-" + dt);
 
+    let json = {
+      domainName: results.domainName,
+      creationDate: date,
+      expiryDate: results.registrarRegistrationExpirationDate,
+      domainAge: domainAge,
+    };
+
+    var xls = json2xls(json);
+
+    fs.writeFileSync(`downloads/${Date.now()}.xlsx`, xls, "binary");
+
+    exec(`python app.py`, (err, stdout, stderr) => {});
+
     res.render("domainwhoisinfo", {
       title:
         "Whois Lookup Info Domain Availability & Registrar Checker - FreeMediaTools.com",
@@ -155,96 +228,20 @@ app.post("/domainwhoisinfo", async (req, res) => {
       flag: true,
       date: date,
       domainAge: domainAge,
+      invalid: false,
     });
   } else {
-    res.send("please enter domain name without http or https");
+    res.render("domainwhoisinfo", {
+      title:
+        "Whois Lookup Info Domain Availability & Registrar Checker - FreeMediaTools.com",
+      data: "",
+      flag: false,
+      date: "",
+      domainAge: "",
+      invalid: true,
+    });
   }
 });
 
 app.listen(7000);
 console.log("app listening on port 7000");
-
-// app.post("/domainwhoisinfo", async (req, res) => {
-//   var domain = req.body.domain;
-//   console.log(domain);
-//   var results = await whoisinfo(domain);
-//   console.log(results);
-//   res.render("domainwhoisinfo", { data: response });
-//   var date = moment(results.creationDate).format("YYYY-MM-DD");
-//   var currentDate = moment(new Date()).format("YYYY-MM-DD");
-
-//   console.log(date);
-//   console.log(currentDate);
-
-//   var a = moment(date);
-//   var b = moment(currentDate);
-
-//   var years = b.diff(a, "year");
-//   a.add(years, "years");
-
-//   var months = b.diff(a, "months");
-//   a.add(months, "months");
-
-//   var days = b.diff(a, "days");
-
-//   var domainAge = years + " years " + months + " months " + days + " days";
-
-//   console.log(years);
-//   console.log(months);
-//   console.log(days);
-
-//   //console.log(year + "-" + month + "-" + dt);
-
-//   res.render("domainwhoisinfo", {
-//     title:
-//       "Whois Lookup Info Domain Availability & Registrar Checker - FreeMediaTools.com",
-//     data: results,
-//     flag: true,
-//     date: date,
-//     domainAge: domainAge,
-//   });
-// });
-
-// app.post("/domainagechecker", async (req, res) => {
-//   var domain = req.body.domain;
-
-//   var results = await whoisinfo(domain);
-
-//   var date = moment(results.creationDate).format("YYYY-MM-DD");
-//   var currentDate = moment(new Date()).format("YYYY-MM-DD");
-
-//   console.log(date);
-//   console.log(currentDate);
-
-//   var a = moment(date);
-//   var b = moment(currentDate);
-
-//   var years = b.diff(a, "year");
-//   a.add(years, "years");
-
-//   var months = b.diff(a, "months");
-//   a.add(months, "months");
-
-//   var days = b.diff(a, "days");
-
-//   var domainAge = years + " years " + months + " months " + days + " days";
-
-//   console.log(years);
-//   console.log(months);
-//   console.log(days);
-
-//   //console.log(year + "-" + month + "-" + dt);
-
-//   res.render("domainagechecker", {
-//     title:
-//       "Check Domain Age Online - Website Age Checker - Domain Age Checker - FreeMediaTools.com",
-//     data: results,
-//     flag: true,
-//     date: date,
-//     domainAge: domainAge,
-//   });
-// });
-
-// app.post('/domainwhoisinfo',async(req,res) => {
-// var
-// })
